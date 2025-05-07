d0870bceb81b703b53ebf5d6427ab7b1f034e0db
Yarik_Killer-Pico <sologubaroslav504@gmail.com>
2025-05-07 12:23:05 +0300

    Create index.html

---
+++ script.js
@@ -0,0 +1,33 @@
+function ToggleAnswer(answer1) {
+    let answer = document.getElementById(answer1);
+    if (answer1) {
+       
+        if (answer.style.display === "none" || answer.style.display === "") {
+            answer.style.display = "block";
+        } else {
+            answer.style.display = "none";
+}
+    }
+}
+function ToggleAnswer(answer2) {
+    let answer = document.getElementById(answer2);
+    if (answer2) {
+       
+        if (answer.style.display === "none" || answer.style.display === "") {
+            answer.style.display = "block";
+        } else {
+            answer.style.display = "none";
+}
+    }
+}
+function ToggleAnswer(answer3) {
+    let answer = document.getElementById(answer3);
+    if (answer3) {
+       
+        if (answer.style.display === "none" || answer.style.display === "") {
+            answer.style.display = "block";
+        } else {
+            answer.style.display = "none";
+}
+    }
+}
